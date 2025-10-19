ce016bd78222447c5152101a988e9cb4b2541693
ishanPamodyaa <inishan9999@gmail.com>
2025-10-19 20:09:53 +0530

    initiealize sample project

---
+++ src/main/java/edu/icet/SpringSecurityWithJWTApp.java
@@ -0,0 +1,12 @@
+package edu.icet;
+
+import com.sun.tools.javac.Main;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityWithJWTApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityWithJWTApp.class);
+    }
+}
